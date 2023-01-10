dda77d56764e0c6411ea16a9e19db12444ba536c
vadimcvirko <promiti90@gmail.com>
2023-01-10 18:42:38 +0300

    context types

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,34 @@
+import React, {FunctionComponent, useState} from "react";
+import {AppProvider, IAppContext} from "../context/AppContext";
+import Alert from "../Components/Alert/Alert";
+import Header from "../Components/Header/Header";
+import {LayoutProps} from "./LayoutProps";
+
+const Layout = ({children}: LayoutProps) => {
+    const [alertName, setAlertName] = useState("");
+
+    const closeAlert = () => {
+        setAlertName("");
+    };
+
+    return (
+        <div>
+            <Header/>
+            <button onClick={() => setAlertName("Click")}> Click</button>
+
+            {alertName && <Alert name={alertName} closeAlert={closeAlert}/>}
+        </div>
+    );
+};
+
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <AppProvider>
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            </AppProvider>
+        );
+    };
+};
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,17 +1,17 @@
-import { createContext, PropsWithChildren, useState } from "react";
+import {createContext, PropsWithChildren, useState} from "react";
 
 export interface IAppContext {
-  menu?: string;
+    menu?: string;
 }
 
 export const AppContext = createContext<IAppContext>({});
 
-export const AppProvider = ({ children }: PropsWithChildren<IAppContext>) => {
-  const [first, setfirst] = useState("");
+export const AppProvider = ({children}: PropsWithChildren<IAppContext>) => {
+    const [first, setfirst] = useState("");
 
-  return (
-    <AppContext.Provider value={{ menu: "123" }}>
-      {children}
-    </AppContext.Provider>
-  );
+    return (
+        <AppContext.Provider value={{menu: "123"}}>
+            {children}
+        </AppContext.Provider>
+    );
 };
