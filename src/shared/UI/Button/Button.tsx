919efc9bde73a7caffad3bf9a36c45fc278f90d6
vadimcvirko <promiti90@gmail.com>
2023-02-18 15:49:27 +0300

    ts config

--- src/shared/UI/Button/Button.tsx
+++ src/shared/UI/Button/Button.tsx
@@ -1,6 +1,14 @@
-import React, {ButtonHTMLAttributes, FC} from 'react';
-import {classNames} from "../../lib/classNames/classNames";
-import styles from './Button.module.css'
+import {ButtonHTMLAttributes, DetailedHTMLProps, FC} from 'react';
+import styles from './Button.module.css';
+import classNames from "classnames";
+
+
+export enum ButtonTheme {
+    CLEAR = 'clear',
+    OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgroundInverted',
+}
 
 export enum ButtonSize {
     M = 'size_m',
@@ -8,36 +16,31 @@ export enum ButtonSize {
     XL = 'size_xl',
 }
 
-export enum ButtonTheme {
-    CLEAR = 'clear',
-    OUTLINE = 'outline',
-    BACKGROUND = 'background',
-    BACKGROUND_INVERTED = 'backgroundInverted',
-
-}
-
-
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    className?: string
-    size?: ButtonSize,
-    theme?: ButtonTheme,
+interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>  {
+    theme?: ButtonTheme;
     square?: boolean;
+    size?: ButtonSize;
 }
 
-const Button: FC<ButtonProps> = (props) => {
-
-    const {children, className = '', size='', square, theme = '', ...otherProps} = props
-
-    const mods: Record<string, boolean> = {
-        [styles[theme]]: true,
-        [styles.square]: square,
-        [styles[size]]: true,
-    };
+export const Button: FC<ButtonProps> = (props) => {
+    const {
+        className,
+        children,
+        theme,
+        square,
+        size = ButtonSize.M,
+        ...otherProps
+    } = props;
     return (
-        <button className={classNames(styles.Button, mods, [className,])} {...otherProps}>
+        <button
+            type="button"
+            className={classNames(styles.Button,className,{
+                [styles[theme]]:true,
+                [styles[size]]:true
+            })}
+            {...otherProps}
+        >
             {children}
         </button>
     );
 };
-
-export default Button;
