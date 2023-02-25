14718cf44622740754842e7d785a1d044c1b2cc8
vadimcvirko <promiti90@gmail.com>
2023-02-25 11:29:28 +0300

    json-server

---
+++ json-server/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const jsonServer = require('json-server')
+const path = require('path')
+
+
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.resolve(__dirname,"db.json"))
+
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+
+server.use(async (req, res, next) => {
+    await new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+    next();
+});
+
+
+server.post('/users',(req,res)=>{
+    return res.json({
+        name:'vadim',
+        id:1
+    })
+})
+
+
+server.use(router)
+
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
